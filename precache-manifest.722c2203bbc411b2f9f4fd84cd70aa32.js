3328f556050ba913c840f12d952362e8f2c64243
Lê Tiến <lengoctien1809@gmail.com>
2020-11-01 09:25:47 +0700

    Updates

---
+++ precache-manifest.722c2203bbc411b2f9f4fd84cd70aa32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8d673badb7aac0a24d264a88c7e4170",
+    "url": "/Booking-movie/index.html"
+  },
+  {
+    "revision": "32b7f37c51ead599292e",
+    "url": "/Booking-movie/static/css/main.fe1c45fe.chunk.css"
+  },
+  {
+    "revision": "c37cfaef430467c48db6",
+    "url": "/Booking-movie/static/js/2.3060d0b0.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/Booking-movie/static/js/2.3060d0b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32b7f37c51ead599292e",
+    "url": "/Booking-movie/static/js/main.3240d715.chunk.js"
+  },
+  {
+    "revision": "1409d149a0a757b08f40",
+    "url": "/Booking-movie/static/js/runtime-main.0f529dff.js"
+  }
+]);
